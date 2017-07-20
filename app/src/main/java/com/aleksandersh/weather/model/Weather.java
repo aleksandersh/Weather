424fcfb2d5377882bc5eadebecab3016c1e85a70
AleksanderSh <alexxx.inter@mail.ru>
2017-07-20 06:16:14 +0300

    Changed Weather model and converter. Removed Location and WeatherCondition models, because they are not using anymore.

--- app/src/main/java/com/aleksandersh/weather/model/Weather.java
+++ app/src/main/java/com/aleksandersh/weather/model/Weather.java
@@ -1,47 +1,26 @@
 package com.aleksandersh.weather.model;
 
-import java.util.Date;
-import java.util.List;
-
 /**
  * Created by AleksanderSh on 14.07.2017.
  */
 
 public class Weather {
-    private Date mCalculationTime;
-    private Location mLocation;
-    private List<WeatherCondition> mConditions;
+    private long mCityId;
     private double mTemperature;
-    private double mMinimumTemperature;
-    private double mMaximumTemperature;
     private int mPressure;
     private int mHumidity;
     private int mCloudiness;
     private double mWindSpeed;
     private int mWindDirection;
+    private String mDescription;
+    private String mGroup;
 
-    public Date getCalculationTime() {
-        return mCalculationTime;
+    public long getCityId() {
+        return mCityId;
     }
 
-    public void setCalculationTime(Date calculationTime) {
-        mCalculationTime = calculationTime;
-    }
-
-    public Location getLocation() {
-        return mLocation;
-    }
-
-    public void setLocation(Location location) {
-        mLocation = location;
-    }
-
-    public List<WeatherCondition> getConditions() {
-        return mConditions;
-    }
-
-    public void setConditions(List<WeatherCondition> conditions) {
-        mConditions = conditions;
+    public void setCityId(long cityId) {
+        mCityId = cityId;
     }
 
     public double getTemperature() {
@@ -50,22 +29,6 @@ public class Weather {
 
     public void setTemperature(double temperature) {
         mTemperature = temperature;
-    }
-
-    public double getMinimumTemperature() {
-        return mMinimumTemperature;
-    }
-
-    public void setMinimumTemperature(double minimumTemperature) {
-        mMinimumTemperature = minimumTemperature;
-    }
-
-    public double getMaximumTemperature() {
-        return mMaximumTemperature;
-    }
-
-    public void setMaximumTemperature(double maximumTemperature) {
-        mMaximumTemperature = maximumTemperature;
     }
 
     public int getPressure() {
@@ -108,22 +71,19 @@ public class Weather {
         mWindDirection = windDirection;
     }
 
-    public String getConditionsDescription() {
-        String conditionText = "";
-        if (!mConditions.isEmpty()) {
-            if (mConditions.size() == 1) {
-                conditionText = mConditions.get(0).getDescription();
-            } else {
-                StringBuilder stringBuilder = new StringBuilder();
-                for (WeatherCondition condition : mConditions) {
-                    if (stringBuilder.length() > 0) {
-                        stringBuilder.append(", ");
-                    }
-                    stringBuilder.append(condition.getDescription());
-                }
-                conditionText = stringBuilder.toString();
-            }
-        }
-        return conditionText;
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public String getGroup() {
+        return mGroup;
+    }
+
+    public void setGroup(String group) {
+        mGroup = group;
     }
 }
