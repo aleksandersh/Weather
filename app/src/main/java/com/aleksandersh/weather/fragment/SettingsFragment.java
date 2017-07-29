2dbac7c98d15b3b6db8fd343488db22dac1180d0
Vladimir Kondenko <kondenko2011@gmail.com>
2017-07-30 00:26:08 +0300

    Fix a package name typo

--- app/src/main/java/com/aleksandersh/weather/fragment/SettingsFragment.java
+++ app/src/main/java/com/aleksandersh/weather/fragment/SettingsFragment.java
@@ -14,6 +14,7 @@ import javax.inject.Inject;
  * Фрагмент, содержащий настройки приложения.
  */
 public class SettingsFragment extends PreferenceFragmentCompat {
+
     @Inject
     SharedPreferences.OnSharedPreferenceChangeListener mChangeListener;
 
@@ -29,7 +30,6 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         ((WeatherApplication) getActivity().getApplication()).getAppComponent().inject(this);
     }
 
