c541b3a499c58fcba8231c39ba2bc58a5854994e
Vladimir Kondenko <kondenko2011@gmail.com>
2017-08-13 15:33:33 +0300

    Remove unused classes and methods

--- app/src/main/java/com/aleksandersh/weather/utils/Utils.java
+++ app/src/main/java/com/aleksandersh/weather/utils/Utils.java
@@ -1,15 +1,11 @@
 package com.aleksandersh.weather.utils;
 
 
-import java.util.Calendar;
-import java.util.Date;
-
 import io.reactivex.Completable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Action;
 import io.reactivex.schedulers.Schedulers;
-import timber.log.Timber;
 
 
 /**
@@ -17,17 +13,6 @@ import timber.log.Timber;
  */
 
 public class Utils {
-
-    public static boolean areOnSameDay(int timestamp1, int timestamp2) {
-        Calendar cal1 = Calendar.getInstance();
-        Calendar cal2 = Calendar.getInstance();
-        cal1.setTime(new Date(timestamp1));
-        cal2.setTime(new Date(timestamp2));
-        boolean result = cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
-                cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR);
-        Timber.i("Dates fall on the same day - " + result);
-        return result;
-    }
 
     public static void transaction(Action action) {
         Completable.fromAction(action)
--- app/src/testSources/java/com/aleksandersh/weather/di/component/TestAppComponent.java
+++
@@ -1,8 +0,0 @@
-package com.aleksandersh.weather.di.component;
-
-/**
- * Created by Vladimir Kondenko on 30.07.17.
- */
-
-public class TestAppComponent  {
-}
